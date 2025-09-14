6bfe3f198182fd790056fd0c50e89dda26419e20
peter2500zz <2797880090@qq.com>
2025-09-14 19:29:20 +0800

    feat: 使用并发处理文件

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,153 @@
-use std::fs;
+use std::{fs, path::PathBuf};
+use bytes::Bytes;
+use clap::Parser;
 use crab_nbt::{Nbt, NbtCompound, NbtTag};
-use mca::{RegionReader, RegionWriter};
+use mca::{RawChunk, RegionReader, RegionWriter};
+use tokio::task::JoinSet;
 use uuid::Uuid;
+use anyhow::Result;
 
-const PATH: &str = "/server/world/entities/r.0.-1.mca";
+const PATH: &str = "/server/world";
 
-fn main() {
-    let f = fs::read(format!(".{PATH}")).unwrap();
+#[derive(Parser)]
+#[command(name = "uuid_remap")]
+#[command(about = "快速改变存档中出现的uuid")]
+struct Args {
+    /// world目录
+    #[arg(long, value_name = "Path of world")]
+    world: String,
+    
+    /// 是否启用详细输出
+    #[arg(short, long)]
+    verbose: bool,
+}
 
-    let region = RegionReader::new(&f).unwrap();
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let a = get_all_mca(&format!(".{PATH}"));
+
+    // println!("{:?}", a);
+    let mut tasks = JoinSet::new();
+
+    for path in a {
+        println!("processing: {:?}", path);
+        tasks.spawn(process_mca(path));
+    }
+
+    while let Some(_) = tasks.join_next().await {}
+    
+    println!("所有文件处理完成");
+}
+
+fn get_all_mca(dir: &str) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+    
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if let Some(extension) = path.extension() && extension == "mca" {
+                files.push(path);
+            } else if path.is_dir() {
+                files.extend(get_all_mca(path.to_str().unwrap()));
+            }
+        }
+    }
+    
+    files
+}
+
+async fn process_mca(path: PathBuf) -> Result<()> {
+    let f = fs::read(path).unwrap();
+
+    let region = match RegionReader::new(&f) {
+        Ok(region) => region,
+        Err(e) => return Ok(())
+    };
     let mut new_region = RegionWriter::new();
 
     for z in 0..=31u8 {
         for x in 0..=31u8 {
             if let Ok(chunk) = region.get_chunk(x as usize, z as usize) && let Some(chunk) = chunk {
-                let c = chunk.decompress().unwrap();
+                let compression_type = chunk.get_compression_type();
 
-                let nbt = Nbt::read(&mut c.as_slice()).unwrap();
+                let data = process_chunk(chunk).unwrap();
 
-                println!("{:?}", nbt);
-
-                let mut root = NbtCompound::new();
-
-                for (key, value) in &nbt.child_tags {
-                    if key == "Entities" {
-                        let mut new_entites = Vec::new();
-                        for i in value.extract_list().unwrap() {
-                            let mut tag = NbtCompound::new();
-                            for (key, value) in &i.extract_compound().unwrap().child_tags {
-                                if key == "UUID" {
-                                    let tv = value.extract_int_array().unwrap();
-                                    let uuid = i32s_to_uuid4(&tv[0..4]);
-
-                                    let v = if uuid.to_string() == "58d99299-2da3-4b95-ab02-9f1056c4fc10" {
-                                        uuid_string_to_i32s("68d99299-2da3-4b95-ab02-9f1056c4fc11").unwrap().to_vec()
-                                    } else {
-                                        tv.clone()
-                                    };
-
-                                    tag.put(key.clone(), v);
-                                } else {
-                                    tag.put(key.clone(), value.clone());
-                                }
-                                
-                            }
-                            new_entites.push(NbtTag::from(tag));
-                        }
-                        root.put(key.clone(), new_entites);
-                    } else {
-                        root.put(key.clone(), value.clone());
-                    }
-                }
-
-                let new = Nbt::new("".to_string(), root).write();
-                println!("{:?}", new);
-                new_region.push_chunk_with_compression(&new, (x, z), chunk.get_compression_type()).unwrap();
+                // new_region.push_chunk_with_compression(&data, (x, z), compression_type).unwrap();
             }
         }
+    };
+
+    Ok(())
+
+    // let mut newf = fs::File::create(format!(".{PATH}")).unwrap();
+    // new_region.write(&mut newf).unwrap();
+}
+
+fn process_chunk(chunk: RawChunk) -> Result<Bytes> {
+    let c = chunk.decompress()?;
+
+    let nbt = Nbt::read(&mut c.as_slice())?;
+
+    // println!("{:?}", nbt);
+
+    let new_nbt = process_compound(
+        &NbtCompound::from(nbt), 
+        |tag_name, nbt_tag| {
+            // 获取实体NBT
+            let new_tag = if tag_name == "Entities" {
+                process_list(
+                    nbt_tag.extract_list()?, 
+                    |nbt_tag| {
+                        // 处理实体NBT
+                        let new_tag = process_compound(
+                            nbt_tag.extract_compound()?, 
+                            |tag_name, nbt_tag| {
+                                if tag_name == "UUID" {
+                                    let uuid = i32s_to_uuid4(nbt_tag.extract_int_array()?);
+                                    // println!("find uuid: {}", uuid);
+                                }
+
+                                Some(nbt_tag.clone())
+                            }
+                        )?;
+
+                        Some(new_tag.into())
+                    }
+                )?.into()
+            } else {
+                nbt_tag.clone()
+            };
+
+            Some(new_tag)
+        }
+    ).unwrap();
+
+    // println!("{:?}", r);
+
+    Ok(Nbt::new("".to_string(), new_nbt).write())
+}
+
+fn process_compound(compound: &NbtCompound, handler: fn(&String, &NbtTag) -> Option<NbtTag>) -> Option<NbtCompound> {
+    let mut new_compound = NbtCompound::new();
+
+    for (tag_name, nbt_tag) in &compound.child_tags {
+        new_compound.put(tag_name.clone(), handler(tag_name, nbt_tag)?);
     }
 
-    let mut newf = fs::File::create(format!(".{PATH}")).unwrap();
-    new_region.write(&mut newf).unwrap();
+    Some(new_compound)
+}
 
-    // println!("{:#?}", );
+fn process_list(list: &Vec<NbtTag>, handler: fn(nbt_tag: &NbtTag) -> Option<NbtTag>) -> Option<Vec<NbtTag>> {
+    let mut new_list = Vec::new();
 
+    for nbt_tag in list {
+        new_list.push(handler(nbt_tag)?);
+    }
+
+    Some(new_list)
 }
 
 fn i32s_to_uuid4(values: &[i32]) -> Uuid {
@@ -93,7 +177,7 @@ fn uuid4_to_i32s(uuid: Uuid) -> [i32; 4] {
     [high, mid_high, mid_low, low]
 }
 
-fn uuid_string_to_i32s(uuid_str: &str) -> Result<[i32; 4], uuid::Error> {
+fn uuid_string_to_i32s(uuid_str: &str) -> Result<[i32; 4]> {
     let uuid = Uuid::parse_str(uuid_str)?;
     Ok(uuid4_to_i32s(uuid))
 }
