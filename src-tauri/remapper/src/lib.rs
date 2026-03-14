0c59aac83863956fababd840c0722b69bc40d8a6
peter2500zz <2797880090@qq.com>
2026-03-14 21:54:06 +0800

    feat(remaper): 实现读取 mca 文件，递归处理其中的 nbt 并写回

---
+++ src-tauri/remapper/src/lib.rs
@@ -0,0 +1,168 @@
+use std::{
+    // collections::HashMap,
+    fs,
+    io::{BufWriter, Write}, str::FromStr,
+};
+
+use anyhow::Result;
+use mca::{RegionReader, RegionWriter};
+use uuid::Uuid;
+
+fn to_u128(a: i32, b: i32, c: i32, d: i32) -> u128 {
+    let a = a as u32 as u128;
+    let b = b as u32 as u128;
+    let c = c as u32 as u128;
+    let d = d as u32 as u128;
+
+    (a << 96) | (b << 64) | (c << 32) | d
+}
+
+fn from_u128(mut value: u128) -> [i32; 4] {
+    let mut parts = [0i32; 4];
+    for i in (0..4).rev() {
+        let part = (value & 0xFFFF_FFFF) as u32;
+        parts[i] = part as i32;
+        value >>= 32;
+    }
+    parts
+}
+
+pub fn i32s_to_uuid4(values: &[i32]) -> Uuid {
+    Uuid::from_u128(to_u128(values[0], values[1], values[2], values[3]))
+}
+
+pub fn uuid4_to_i32s(uuid: Uuid) -> [i32; 4] {
+    from_u128(uuid.as_u128())
+}
+
+/// 对于每个 NBT，会递归调用此函数以处理可能的所有值
+/// 
+/// 触发递归的只有列表和复合标签
+fn process_nbt(nbt: &mut fastnbt::Value) {
+    match nbt {
+        // fastnbt::Value::Byte(_) => todo!(),
+        // fastnbt::Value::Short(_) => todo!(),
+        // fastnbt::Value::Int(_) => todo!(),
+        // fastnbt::Value::Long(_) => todo!(),
+        // fastnbt::Value::Float(_) => todo!(),
+        // fastnbt::Value::Double(_) => todo!(),
+        fastnbt::Value::String(v) => {
+            if v == "minecraft:diamond_hoe" {
+                *v = "minecraft:diamond_axe".to_string();
+                println!("{}", v);
+            } else if v == "minecraft:diamond_axe" {
+                *v = "minecraft:diamond_hoe".to_string();
+                println!("{}", v);
+            }
+        }
+        // fastnbt::Value::ByteArray(byte_array) => todo!(),
+        fastnbt::Value::IntArray(int_array) => {
+            if int_array.len() == 4 {
+                let old_uuid = i32s_to_uuid4(int_array);
+
+                if old_uuid.to_string().to_lowercase() == "9db4226c-1015-40da-8fa5-4335aab896b6" {
+                    println!("{}", old_uuid);
+
+                    let new_uuid = uuid4_to_i32s(Uuid::from_str("59c66d96-d356-364a-a84e-0511b286a31b").unwrap());
+
+                    *int_array = fastnbt::IntArray::new(new_uuid.to_vec());
+                }
+            }
+        }
+        // fastnbt::Value::LongArray(long_array) => todo!(),
+        fastnbt::Value::List(values) => {
+            for item in values {
+                process_nbt(item);
+            }
+        }
+        fastnbt::Value::Compound(hash_map) => {
+            for v in hash_map.values_mut() {
+                process_nbt(v);
+            }
+            // drain 保留了未来修改 key 的灵活性
+            // let new_map = hash_map
+            //     .drain()
+            //     .map(|(k, mut v)| {
+            //         process_nbt(&mut v);
+            //         (k, v)
+            //     })
+            //     .collect();
+            // *hash_map = new_map;
+        }
+        _ => (),
+    }
+}
+
+/// 解析 mca 文件，并提取其中的区块与 NBT 数据
+pub fn process_mca(mca_path: &str) -> Result<()> {
+    let mca_file = fs::read(mca_path)?;
+
+    let mut region = RegionReader::new(&mca_file)?;
+    let mut new_region = RegionWriter::new();
+
+    for (x, z) in region.generated_chunks()? {
+        // 上面已经只遍历了已生成的区块，这里正常来说不会为 None
+        if let Some(chunk) = region.chunk_data(x, z)? {
+            // println!("Processing chunk at ({}, {})", x, z);
+            let compression_type = chunk.compression.clone();
+            let buffer = region.decompress_to_internal_buffer(chunk)?;
+
+            let mut nbt: fastnbt::Value = fastnbt::from_bytes(buffer)?;
+            // println!("Original NBT: {:#?}", nbt);
+            // let mut input = String::new();
+            // std::io::stdin().read_line(&mut input).unwrap();
+            process_nbt(&mut nbt);
+
+            let new_nbt = fastnbt::to_bytes(&nbt)?;
+
+            assert!(
+                buffer.len() == new_nbt.len(),
+                "NBT数据长度发生了变化，无法写回原区块"
+            );
+
+            new_region.set_chunk(x, z, new_nbt, compression_type)?;
+        } else {
+            panic!("bad chunk? is the mca file damaged?")
+        }
+    }
+
+    let file = fs::File::create(format!("{}.mod", mca_path))?;
+    let mut writer = BufWriter::new(file);
+
+    new_region.write(&mut writer)?;
+    writer.flush()?;
+
+    Ok(())
+}
+
+#[test]
+fn mca() -> Result<()> {
+    // let file =
+    //     fs::read(r"C:\Users\27978\Downloads\新建文件夹\server\world\region\r.0.0.mca").unwrap();
+    let start_time = std::time::Instant::now();
+    process_mca(r"C:\Users\27978\Downloads\新建文件夹\server\world\entities\r.0.0.mca")?;
+    let duration = start_time.elapsed();
+    println!("Time taken: {:?}", duration);
+
+    // Uuid::from_u128(v)
+
+    // if let Ok(mut region) = RegionReader::new(&file) {
+    //     for (x, z) in region.generated_chunks().unwrap() {
+    //         let chunk = region.chunk(x, z).unwrap().unwrap();
+    //         let mut nbt: fastnbt::Value = fastnbt::from_bytes(chunk).unwrap();
+
+    //         process_nbt(&mut nbt);
+
+    //         // println!("{:#?}", nbt);
+
+    //         // wait for enter
+    //         // let mut input = String::new();
+    //         // std::io::stdin().read_line(&mut input).unwrap();
+    //         // println!("{}")
+    //     }
+    // } else {
+    //     println!("Failed to read the region file.");
+    // }
+
+    Ok(())
+}
